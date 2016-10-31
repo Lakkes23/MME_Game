4bf5af1e54266be837d52b968064d59931088b3a
janschill <schilllll@googlemail.com>
2016-10-31 20:27:24 +0100

    fullscreen

--- app/src/main/java/com/hsfl/mme/mme_game/MainActivity.java
+++ app/src/main/java/com/hsfl/mme/mme_game/MainActivity.java
@@ -1,13 +1,30 @@
 package com.hsfl.mme.mme_game;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    public final static String EXTRA_MESSAGE = "com.hsfl.mme.mme_game.MESSAGE";
+
+    private Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+    }
+
+    public void startGameActivity(View view)
+    {
+        Intent intent = new Intent(this, GameActivity.class);
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
